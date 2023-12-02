64037a356d0c35e896e5df260ea3257d2f026bf5
joach <joachim.lerchenmueller@alumni.fh-aachen.de>
2023-12-02 18:17:21 +0100

    test

---
+++ mysite/polls/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+from django.utils import timezone
+from datetime import datetime, timedelta
+from django.contrib import admin
+from filer.fields.file import *
+from filer.fields.image import *
+
+# Create your models here.(
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField("Date Published")
+
+    def __str__(self):
+        return self.question_text
+    
+    @admin.display(
+        boolean=True,
+        ordering="pub_date",
+        description="Published recently?",
+    )
+
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - timedelta(days=1) <= self.pub_date <= now
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
+    
